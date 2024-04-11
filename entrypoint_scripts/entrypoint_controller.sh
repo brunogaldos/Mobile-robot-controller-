d5dce6027fcf4ce7c8a1cd0324901135997b6a4c
Bruno Galdos <95909869+brunogaldos@users.noreply.github.com>
2024-04-11 13:34:43 +0200

    Update entrypoint_controller.sh

--- entrypoint_scripts/entrypoint_controller.sh
+++ entrypoint_scripts/entrypoint_controller.sh
@@ -10,8 +10,5 @@ source /colcon_ws/install/setup.bash
 
 # Run additional commands
 
-#ros2 run my_controller trial_controller_opti &
-#ros2 run my_controller evaluation_node_odom
-#ros2 run my_controller simplenode
-#ros2 run my_controller trial_controller_backup_original
+ros2 launch my_bot launch_sim.launch.py &
 ros2 run my_controller trial_controller_diff
